86a94c5599c80fdc44c58ecc21068b0f3a6068d9
Aleksandar Todorov <alex.todorov86@gmail.com>
2020-09-10 01:02:18 +0300

    worked on url2domain_name.py

--- url2domain_name.py
+++ url2domain_name.py
@@ -1,15 +1,22 @@
 import re
 
 url = input()
-def url_2_domainname(string):
+
+
+def domain_name(string):
     www_regex = '(?<=www\.)([a-zA-Z\-0-9]*)'
     http_regex = '(?<=://)([a-zA-Z\-0-9]*)'
+    # The one below doesn't work - must be fixed
+    no_prefix_regex = '/([a-zA-Z\-0-9]*(?=\.))/'
     new_string = ''
     if 'www' in string:
         new_string = re.findall(www_regex, string)
-    else:
+    elif 'http' in string:
         new_string = re.findall(http_regex, string)
+    else:
+        new_string = re.findall(no_prefix_regex, string)
 
-    return str(new_string[0])
+    return new_string.pop()
 
-print(url_2_domainname(url))
+
+print(domain_name(url))
