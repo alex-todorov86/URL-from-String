d5706ba4b6ddd838cdcfaeaa4ea0080a47bbbb77
Aleksandar Todorov <alex.todorov86@gmail.com>
2020-09-09 23:31:29 +0300

    added url2domain function

--- url2domain_name.py
+++ url2domain_name.py
@@ -1,7 +1,15 @@
 import re
 
-
+url = input()
 def url_2_domainname(string):
-    regex = ''
+    www_regex = '(?<=www\.)([a-zA-Z\-0-9]*)'
+    http_regex = '(?<=://)([a-zA-Z\-0-9]*)'
     new_string = ''
-    return new_string
+    if 'www' in string:
+        new_string = re.findall(www_regex, string)
+    else:
+        new_string = re.findall(http_regex, string)
+
+    return str(new_string[0])
+
+print(url_2_domainname(url))
