4ae5a225979a1a7e8efed8e7d4f6d8948b19c1a8
alex-todorov86 <67958047+alex-todorov86@users.noreply.github.com>
2020-09-10 10:47:48 +0300

    Update url2domain_name.py
     # Although this covers the test cases it's not a good solution. 
     # Consider traversing the string backwards.

--- url2domain_name.py
+++ url2domain_name.py
@@ -7,7 +7,7 @@ def domain_name(string):
     www_regex = '(?<=www\.)([a-zA-Z\-0-9]*)'
     http_regex = '(?<=://)([a-zA-Z\-0-9]*)'
     # The one below doesn't work - must be fixed
-    no_prefix_regex = '/([a-zA-Z\-0-9]*(?=\.))/'
+    no_prefix_regex = '([a-zA-Z\-0-9]+(?=\.))'
     new_string = ''
     if 'www' in string:
         new_string = re.findall(www_regex, string)
@@ -15,8 +15,12 @@ def domain_name(string):
         new_string = re.findall(http_regex, string)
     else:
         new_string = re.findall(no_prefix_regex, string)
-
-    return new_string.pop()
+    
+   
+    if len(new_string) > 1:
+        new_string = new_string.pop(1)
+    else:
+        new_string = new_string.pop()
 
 
 print(domain_name(url))
